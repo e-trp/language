18294e9682fb7b50a860d5fafa10df2055acbb28
EvgeniyToropchin <e.toropchin@inlinetelecom.ru>
2025-04-16 13:29:17 +0300

    change layout by menu click

--- src/gui.rs
+++ src/gui.rs
@@ -35,8 +35,8 @@ pub enum MenuItem  {
 #[derive(Debug, Clone)]
 pub struct AppState {
     pub content: String,
-    pub layout: MenuItem,
-    pub result_string: String
+    pub menu: MenuItem,
+    pub search_string: String
 }
 
 
@@ -44,8 +44,8 @@ impl Default for  AppState {
     fn default() -> Self {
         Self {
             content: "".to_string(), 
-            layout: MenuItem::Dictionary(None),
-            result_string: "".to_string(), 
+            menu: MenuItem::Dictionary(None),
+            search_string: "".to_string(), 
 
         } 
     }
@@ -76,8 +76,8 @@ impl AppState {
 
         let menu_dictionary = menu_bar!(
             (debug_button_s("Cловарь"), menu_tpl_1(menu_items!(
-                (labeled_button("Формы глаголов", Message::MenuButton(MenuItem::IrrQuiz(None))))
-                (debug_button("Найти слово"))
+                (labeled_button("Формы глаголов", Message::MenuButton(MenuItem::Finder(None))))
+                (labeled_button("Найти слово", Message::MenuButton(MenuItem::Dictionary(None))))
             )).width(240.0))
         );
 
@@ -91,7 +91,7 @@ impl AppState {
         .padding(10);
 
 
-        match &self.layout {
+        match &self.menu {
             MenuItem::Finder(_) => {
                 let find_view = window.extend( 
                     [
@@ -101,7 +101,7 @@ impl AppState {
                             .on_input(Message::ContentChanged),
                         button("Искать").on_press(Message::SearchButtonPressed),
                         ].spacing(10).into(),
-                        text(&self.result_string).into()
+                        text(&self.search_string).into()
                     ]
                 );
                 return find_view;
@@ -116,7 +116,7 @@ impl AppState {
                         .on_input(Message::ContentChanged),
                     button("Искать").on_press(Message::DictionaryButtonPressed),
                     ].spacing(10).into(),
-                    text(&self.result_string).into()
+                    text(&self.search_string).into()
                 ]);
                 return  dictionary_view;
             }
@@ -130,7 +130,7 @@ impl AppState {
                 self.content = content;
             }
             Message::SearchButtonPressed => {
-                let mut result_string = String::new();
+                let mut search_string = String::new();
                 if let Ok(search_result) = Word::search_irregular_verb_from_db(&self.content) {
                     for (_word, irregular_verb) in search_result {
                         debug!("found irregular verb: {}", &irregular_verb.base_form);
@@ -140,17 +140,28 @@ impl AppState {
                             irregular_verb.past_simple,
                             irregular_verb.past_participle
                         );
-                        result_string.push_str(&table_string);
+                        search_string.push_str(&table_string);
                     }
                 }
-                self.result_string = result_string;
+                self.search_string = search_string;
             }
             Message::DictionaryButtonPressed => {
                 if let Ok(word) = Word::from_str(&self.content) {
-                    self.result_string = word.description.unwrap();
+                    self.search_string = word.description.unwrap();
+                    self.menu = MenuItem::Finder(None);
                 };
             },
-            Message::MenuButton(state) => {
+            Message::MenuButton(menu_item) => {
+                match menu_item {
+                    MenuItem::Finder(_) => {
+                        self.menu = MenuItem::Finder(None);
+                    },
+                    MenuItem::Dictionary(_) => {
+                        self.menu = MenuItem::Dictionary(None);
+                    }, 
+                    _ => {}
+                }
+
             }
             Message::Debug(var) => { debug!("{}", var);}
         }
@@ -171,7 +182,7 @@ fn labeled_button(
     label: &str,
     msg: Message,
 ) -> button::Button<Message, iced::Theme, iced::Renderer> {
-    base_button(text(label).align_y(alignment::Vertical::Center), msg)
+    base_button(text(label).align_y(alignment::Vertical::Center), msg).width(Length::Fill)
 }
 
 fn debug_button(label: &str) -> button::Button<Message, iced::Theme, iced::Renderer> {
