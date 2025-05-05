9de3a30927f9c3f6509474bc7dcfc63e16c72b6f
EvgeniyToropchin <e.toropchin@inlinetelecom.ru>
2025-05-05 16:56:47 +0300

    quiz rework

--- src/quiz.rs
+++ src/quiz.rs
@@ -1,10 +1,10 @@
 use diesel::prelude::*;
-use crate::word::Word;
+use crate::word::{VerbForms, Word};
 use rand::prelude::*;
 use log::debug;
 
 
-#[derive(Debug, Queryable, Selectable, Insertable)]
+#[derive(Debug, Queryable, Selectable, Insertable, Clone)]
 #[diesel(table_name = crate::schema::quiz)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Quiz {
@@ -13,7 +13,7 @@ pub struct Quiz {
 }
 
 
-#[derive(Debug, Queryable, Selectable, Insertable)]
+#[derive(Debug, Queryable, Selectable, Insertable, Clone)]
 #[diesel(table_name = crate::schema::quiz_history)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct QuizHistory {
@@ -31,27 +31,27 @@ pub enum QuizType {
 }
 
 
-impl  Quiz {
-    
-    pub fn new(name: &str) -> Self {
-        Self{id: None, name: name.to_string()}
+#[derive(Debug, Clone)]
+pub struct IrregVerbQuiz {
+    pub quiz: Quiz,
+    pub words: Vec<VerbForms>,
+    pub rand_indexes: Vec<usize>
+}
+
+
+impl IrregVerbQuiz {
+
+    pub fn new() -> Self {
+        let words = Word::fetch_irregular_verbs().unwrap();
+        let mut rand_indexes = (0..words.len()).collect::<Vec<usize>>();
+        rand_indexes.shuffle(&mut rand::rng());
+        let quiz = Quiz{ id: None, name: "irregular verb".to_string()};
+        Self{quiz, words, rand_indexes}
     }
 
-    pub fn run_quiz(&self) {
-        match self.name.as_str() {
-            "irregular_verbs" => {
-                if let Ok(words)= Word::fetch_irregular_verbs() {
-                    let mut shuffle_indexes = (0..words.len()).collect::<Vec<usize>>();
-                    shuffle_indexes.shuffle(&mut rand::rng());
-                    for i in shuffle_indexes {
-                        debug!("{:?}", words[i])
-                    }
-                }
-
-            },
-            "text" => {panic!("not implemented")}
-            _ => {panic!("not implemented")}
-        }
+    pub fn next_word(&mut self) -> Option<VerbForms> {
+        let random_index = self.rand_indexes.pop();
+        match random_index {None => None, Some(i) => Some(self.words[i].clone())}
     }
 
 
